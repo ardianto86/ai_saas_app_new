52318ed8ee9260117dc1fe88231bef62650779ff
ardianto86 <ardianto86@gmail.com>
2024-03-11 17:33:02 +0800

    fix dbname

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -20,7 +20,7 @@ export const connectToDatabase = async() => {
     if (cached.conn) return cached.conn;
     if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
     cached.promise = cached.promise || mongoose.connect(
-        MONGODB_URL, {dbName: 'imaginifyTest1', bufferCommands: false}
+        MONGODB_URL, {dbName: 'ImaginifyTest1', bufferCommands: false}
     )
 
     cached.conn = await cached.promise;
